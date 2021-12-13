1d0e56ab1050b35f867de8ddc1cc1be7cf7c20ec
raasalgul <k.sathishrsurya@gmail.com>
2021-12-13 04:10:31 +0000

    institute fix

--- userLogin/signIn.py
+++ userLogin/signIn.py
@@ -9,6 +9,8 @@ from dotenv import load_dotenv
 ''' Loading Environment files '''
 load_dotenv()
 
+dynamoDbResource = boto3.resource(os.getenv("AWS_DYNAMO"), region_name=os.getenv("AWS_REGION"))
+
 ''' Configuring AWS Cognito '''
 cognitoClient = boto3.client(os.getenv("AWS_COGNITO"), region_name=os.getenv("AWS_REGION"))
 table_name = os.getenv("DYNAMO_USER_TABLE")
@@ -32,6 +34,11 @@ def signIn():
     # responseUserData = cognitoClient.get_user(AccessToken=access_token)
     # print("Response user data {}".format(responseUserData))
     # logging.info("Response user data {}".format(responseUserData))
-
+    table = dynamoDbResource.Table(table_name)
+    institution=""
+    items = table.scan()['Items']
+    for item in items:
+        if item['email'] == request.json['email']:
+            institution = item['institution']
     # return jsonify(response)
-    return jsonify({"token":response['AuthenticationResult']['AccessToken']})
+    return jsonify({"token":response['AuthenticationResult']['AccessToken'],"institution":institution})
